cc62906ecc2bbf3cdd9fb890b0f271de01f588c4
楠　大輝 <daiki4386@gmail.com>
2026-05-13 18:06:16 +0900

    feat: Playerモデルに座標フィールドを追加

--- models/player.go
+++ models/player.go
@@ -1,9 +1,11 @@
 package models
 
 type Player struct {
-	ID       string `gorm:"primaryKey"`
-	RoomID   string `gorm:"index"`
+	ID       string  `gorm:"primaryKey"`
+	RoomID   string  `gorm:"index"` // どの部屋にいるか検索しやすくする
 	Name     string
-	Role     int    // 0:逃走者, 1:鬼
-	IsCaught bool   `gorm:"default:false"`
+	Role     int     // 0:逃走者, 1:鬼
+	IsCaught bool    `gorm:"default:false"`
+	Lat      float64 // 緯度 (Latitude)
+	Lng      float64 // 経度 (Longitude)
 }
