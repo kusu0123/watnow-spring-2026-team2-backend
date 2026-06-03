c79f0cc48c93e7c7b05840a48989d3eea940f90b
Kosuke <heitiankangjie487@gmail.com>
2026-06-03 11:13:05 +0900

    feat: persist player state for reconnect

--- models/player.go
+++ models/player.go
@@ -1,11 +1,13 @@
 package models
 
 type Player struct {
-	ID       string  `gorm:"primaryKey"`
-	RoomID   string  `gorm:"index"` // どの部屋にいるか検索しやすくする
+	ID       string `gorm:"primaryKey"`
+	RoomID   string `gorm:"index:idx_players_room_user,unique"`
+	UserID   string `gorm:"index:idx_players_room_user,unique"`
 	Name     string
 	Role     int     // 0:逃走者, 1:鬼
 	IsCaught bool    `gorm:"default:false"`
 	Lat      float64 // 緯度 (Latitude)
 	Lng      float64 // 経度 (Longitude)
+	Color    string
 }
